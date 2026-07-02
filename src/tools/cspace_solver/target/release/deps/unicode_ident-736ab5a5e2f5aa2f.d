c16977a9d31a28199feb653395846dcf2e4bd69e
otrebor-solrac <solrac_solrac@live.com.mx>
2026-07-02 13:18:01 -0500

    chore: stop tracking Rust build target directory and ignore it

--- src/tools/cspace_solver/target/release/deps/unicode_ident-736ab5a5e2f5aa2f.d
+++
@@ -1,8 +0,0 @@
-/home/ros_ws/src/tools/cspace_solver/target/release/deps/libunicode_ident-736ab5a5e2f5aa2f.rmeta: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.24/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.24/src/tables.rs
-
-/home/ros_ws/src/tools/cspace_solver/target/release/deps/libunicode_ident-736ab5a5e2f5aa2f.rlib: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.24/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.24/src/tables.rs
-
-/home/ros_ws/src/tools/cspace_solver/target/release/deps/unicode_ident-736ab5a5e2f5aa2f.d: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.24/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.24/src/tables.rs
-
-/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.24/src/lib.rs:
-/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.24/src/tables.rs:
